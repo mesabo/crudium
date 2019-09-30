28c946219ae4e85922d736ab98af9dead7018b7e
mesabo <messouaboya17@gmail.com>
2019-09-30 16:55:01 +0000

    Create and test by Mesabo Prodigit

--- lib/database/model/user.dart
+++ lib/database/model/user.dart
@@ -1,6 +1,32 @@
 class User {
   int id;
-  String _firstNAme;
-  String _lastNAme;
+  String _firstName;
+  String _lastName;
   String _dob;
+
+  User(this._firstName, this._lastName, this._dob);
+
+  User.map(dynamic obj){
+    this._firstName = obj["firstName"];
+    this._lastName = obj["lastName"];
+    this._dob = obj["dob"];
+  }
+
+  String get firstName => _firstName;
+
+  String get lastName => _firstName;
+
+  String get dob => _dob;
+
+  Map<String, dynamic> toMap() {
+    var map = new Map<String, dynamic>();
+    map["firstname"] = _firstName;
+    map["lastname"] = _lastName;
+    map["dob"] = dob;
+    return map;
+  }
+
+  void setUserId(int id) {
+    this.id = id;
+  }
 }
