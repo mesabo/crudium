f02aa93b215099e2175a88c3191e421adb250de4
mesabo <messouaboya17@gmail.com>
2019-09-30 16:46:37 +0000

    Create and test by Mesabo Prodigit

---
+++ lib/database/database_helper.dart
@@ -0,0 +1,48 @@
+import 'dart:async';
+
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import 'dart:io' as io;
+
+class DatabaseHelper {
+  static final DatabaseHelper _instance = new DatabaseHelper.internal();
+
+  factory DatabaseHelper() => _instance;
+  static Database _db;
+
+  Future<Database> get db async {
+    if (_db != null) return _db;
+    _db = await initDb();
+    return _db;
+  }
+
+  DatabaseHelper.internal();
+
+  initDb() async {
+    io.Directory documentDirectory = await getApplicationDocumentsDirectory();
+    String path = join(documentDirectory.path, "main.db");
+    var theDb = await openDatabase(path, version: 1, onCreate: _onCreate);
+  }
+
+  void _onCreate(Database db, int version) async {
+//    WHEN CREATING DB CREATEE TABLE
+    await db.execute(
+        "CREATE TABLE User(id INTEGER PRIMARY KEY,firstName TEXT,latsName TEXT,dob TEXT)");
+  }
+
+  Future<int> SaveUser(User user) async {
+    var dbClient = await db;
+    int res = await dbClient.insert("User", user.toMap());
+    return res;
+  }
+
+  Future<List<User>> getUser() async {
+    var dbClient = await db;
+    List<Map> list = await dbClient.rawQuery('SELECT * FROM User');
+    List<User> employees = new List();
+    for (int i = 0; i < list.length; i++) {
+      var user = new User()
+    }
+  }
+}
---
+++ lib/database/model/user.dart
@@ -0,0 +1,6 @@
+class User {
+  int id;
+  String _firstNAme;
+  String _lastNAme;
+  String _dob;
+}
